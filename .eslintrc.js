9323d3af1a1c0a144f78d1531daabd094ebc941e
Sooraj Bhaskaran <sbkaiprath99@gmail.com>
2024-09-22 21:43:47 +0530

    Decoupled neeto-package dependencies (#48)
    * Removed unwanted configurations
    
    * Added the beta version at package json file
    
    * Added prelease token to workflows
    
    * Updated the tag
    
    * Removed neeto-commons-frontend
    
    * Reverted the version
    
    * Removed the dependency on eslint plugin neeto
    
    * Removed eslint package
    
    ---------
    
    Co-authored-by: Joseph Mathew <josephmathew900@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
-const defaultConfigurations = require("@bigbinary/neeto-commons-frontend/configs/nanos/eslint/index.js");
 const { mergeDeepLeft } = require("ramda");
+
+const defaultConfigurations = require("./configs/eslint");
 
 module.exports = mergeDeepLeft(
   {
     extends: [...defaultConfigurations.extends, "plugin:storybook/recommended"],
-    rules: {
-      "@bigbinary/neeto/no-missing-localization": "off",
-      "@bigbinary/neeto/file-name-and-export-name-standards": "off",
-      "@bigbinary/neeto/no-axios-import-outside-apis": "off",
-      "@bigbinary/neeto/use-neetoui-classes": "error",
-      "import/extensions": "off",
-    },
+    rules: { "import/extensions": "off" },
   },
   defaultConfigurations
 );
